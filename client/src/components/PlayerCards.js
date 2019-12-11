4b5c4430deb3d89ba6390ddbbe2ea570ff193bb9
Kira <kawoods826@gmail.com>
2019-12-11 12:41:38 -0800

    added data call and integrated data to create one card

--- client/src/components/PlayerCards.js
+++ client/src/components/PlayerCards.js
@@ -1,14 +1,31 @@
 import React, { Component } from "react";
 import "./PlayerCards.css";
+import axios from "axios";
 
 export class PlayerCards extends Component {
+  state = {
+    playerInfo: []
+  };
+
+  componentDidMount() {
+    axios.get(`http://localhost:5000/api/players`).then(res => {
+      const players = res.data;
+      this.setState({ playerInfo: players });
+    });
+  }
+
   render() {
-    return (
-      <div className="player-card">
-        <h2>Player Name</h2>
-        <p>Country</p>
-        <p>Searches</p>
-      </div>
-    );
+    if (this.state.playerInfo.length === 0) {
+      return <p>Loading...</p>;
+    } else {
+      console.log(this.state.playerInfo);
+      return (
+        <div className="player-card">
+          <h2>{this.state.playerInfo[0].name}</h2>
+          <p>{this.state.playerInfo[0].country}</p>
+          <p>{this.state.playerInfo[0].searches}</p>
+        </div>
+      );
+    }
   }
 }
