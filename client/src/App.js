416b7665ea9da4beeb7c29f0aa0bd7411c40af73
Kira <kawoods826@gmail.com>
2019-12-11 10:29:45 -0800

    added basic component for player cards

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import { PlayerCard } from "./components/PlayerCard";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Women's World Cup Most Searched Players</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Women's World Cup Most Searched Players</h1>
+        <PlayerCard />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/components/PlayerCard.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class PlayerCard extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Player Name</h2>
+        <p>Country</p>
+        <p>Searches</p>
+      </div>
+    );
+  }
+}
