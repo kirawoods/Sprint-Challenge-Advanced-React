3b0cab4f22ca0d8e3bd1d563afb4dd58f8b31440
Kira <kawoods826@gmail.com>
2019-12-18 09:50:09 -0800

    added tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "@testing-library/jest-dom/extend-expect";
+import { render, cleanup } from "@testing-library/react";
+import waitForExpect from "wait-for-expect";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+afterEach(cleanup);
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders "World Cup Most Searched Players" text', () => {
+  const wrapper = render(<App />);
+  const hasWorldCupText = wrapper.queryByText(
+    /world cup most searched players/i
+  );
+  expect(hasWorldCupText).toBeInTheDocument();
+  console.log(wrapper.debug());
 });
