a8a0e2319d2b672f69b1b49209557f8aff33f0b8
Kira <kawoods826@gmail.com>
2019-12-18 10:13:41 -0800

    installed styling dependencies and added toggle button for light and dark mode

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,38 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import "./App.css";
 import { PlayerCards } from "./components/PlayerCards";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Women's World Cup Most Searched Players</h1>
-        <div className="card-container">
-          <PlayerCards />
+const GlobalStyle = createGlobalStyle`
+body {
+  background-color: ${props => (props.theme.mode === "dark" ? "#111" : "#EEE")};
+  color: ${props => (props.theme.mode === "dark" ? "#EEE" : "#111")};
+}`;
+
+function App() {
+  const [theme, setTheme] = useState({ mode: "dark" });
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyle />
+        <div className="App">
+          <h1>Women's World Cup Most Searched Players</h1>
+          <button
+            onClick={e =>
+              setTheme(
+                theme.mode === "dark" ? { mode: "light" } : { mode: "dark" }
+              )
+            }
+          >
+            Toggle Theme
+          </button>
+          <div className="card-container">
+            <PlayerCards />
+          </div>
         </div>
-      </div>
-    );
-  }
+      </>
+    </ThemeProvider>
+  );
 }
 
 export default App;
